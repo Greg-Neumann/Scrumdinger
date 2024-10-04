0220a9c97d31ba5d5ea75c48c2c468c82e138a61
Greg Neumann on his iMac <GregInTheNorth@iCloud.com>
2024-10-04 14:55:07 +0100

    WIP

---
+++ Scrumdinger/ScrumsView.swift
@@ -0,0 +1,18 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Greg Neumann on 04/10/2024.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScrumsView()
+}
