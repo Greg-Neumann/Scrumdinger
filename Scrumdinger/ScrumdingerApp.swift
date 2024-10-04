e339b73880985cfd01a00555bf7a48dd6ce71b4a
Greg Neumann on his iMac <GregInTheNorth@iCloud.com>
2024-10-04 11:21:02 +0100

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Greg Neumann on 04/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
